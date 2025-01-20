2932f8c119544e0b606f36f2d17c4b56d1e9bf13
komodoce <komodoce23@gmail.com>
2025-01-20 15:00:44 +0000

    CE

--- lib/mm2/mm2_web.dart
+++ lib/mm2/mm2_web.dart
@@ -35,7 +35,7 @@ class MM2Web extends MM2 implements MM2WithInit {
   Future<void> start(String? passphrase) async {
     final Map<String, dynamic> params = await MM2.generateStartParams(
       passphrase: passphrase,
-      gui: 'web_dex web',
+      gui: 'web_dex CE',
       dbDir: null,
       userHome: null,
     );
